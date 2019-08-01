828397895036fec5ded595a15fbcee76f0d8c293
gggg2221 <poasboy87@gmail.com>
2019-08-01 14:10:23 +0800

    0801

--- common/mockv.py
+++ common/mockv.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
-from flask import Flask
+from flask import Flask,Response
 import json
 
 app = Flask(__name__)
 
-
 @app.route('/test/',methods =['POST','GET'])
 def hello_world():
+    p1="18022233311"
+    p2="18813574284"
     res = {
         "resultCode": "0",
         "message": "success",
         "obj": {
-            "telephone": "18813574284",
-            "name": "法师"
+            "phone": p1,
+            "name": "杨伟"
         }
     }
-    return json.dumps(res, ensure_ascii=False).encode('utf-8')
+    s=Response(json.dumps(res,ensure_ascii=False).encode('utf-8'), content_type='application/json')
+    return s
 
 if __name__ == '__main__':
     app.run(host='10.101.90.228',port=5000,debug=True)
